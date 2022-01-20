d80d1716074751b786bef77f19f4a1fa65957e92
Eugene Borowski <e.borovskiy@yandex.ru>
2022-01-20 18:12:25 +0300

    Lesson 2 заглушка

---
+++ OOP_Course/Lesson2/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Course.Lesson2
+{
+    internal class BankAccount
+    {
+        private int accountNumber;
+        private int balance;
+        private enum AccountType
+        {
+            Расчетный,
+            Кредитный,
+            Сберегательный
+        };
+
+    }
+}
