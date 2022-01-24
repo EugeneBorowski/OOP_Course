540bd32b3406dc74b6e569884463b7a46631673e
Eugene <e.borovskiy@yandex.ru>
2022-01-24 18:49:58 +0300

    done

---
+++ OOP_Course/Lesson2/2-3/BankAccount3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace OOP_Course.Lesson2._2_3
+{
+    public class BankAccount3
+    {
+        private static int _id;
+        private int _accountId;
+        private decimal _balance;
+        internal enum AccountType
+        {
+            Дебетовый,
+            Кредитный,
+            Сберегательный
+        };
+        private AccountType _accountType;
+
+        internal BankAccount3()
+        {
+            GenerateAccountId();
+            _balance = 0;
+        }
+
+        internal BankAccount3(decimal balance)
+        {
+            GenerateAccountId();
+            _balance = balance;
+        }
+        internal BankAccount3(AccountType accountType)
+        {
+            GenerateAccountId();
+            _balance = 0;
+            _accountType = accountType;
+        }
+        internal BankAccount3(decimal balance,AccountType accountType)
+        {
+            GenerateAccountId();
+            _balance = balance;
+            _accountType = accountType;
+        }
+
+        internal void SetBalance(decimal balance)
+        {
+            _balance = balance;
+        }
+        internal void SetAccountType(AccountType accountType)
+        {
+            _accountType = accountType;
+        }
+
+        internal void PrintAccountDetails()
+        {
+            Console.WriteLine("Информация о счете:");
+            Console.WriteLine("Номер счета: " + _accountId);
+            Console.WriteLine("Баланс: " + _balance);
+            Console.WriteLine("Тип счета: " + _accountType);
+        }
+        private void GenerateAccountId()
+        {
+            _accountId = ++_id;
+        }
+    }
+}
---
+++ OOP_Course/Lesson2/2-4/BankAccount4.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OOP_Course.Lesson2._2_4
+{
+    public class BankAccount4
+    {
+        private static int _id;
+        private int _accountId;
+        private decimal _balance = 0;
+        internal enum AccountType
+        {
+            Дебетовый,
+            Кредитный,
+            Сберегательный
+        };
+        private AccountType _accountType;
+
+        internal BankAccount4()
+        {
+            GenerateAccountId();
+        }
+
+        internal BankAccount4(decimal balance) : this()
+        {
+            _balance = balance;
+        }
+
+        internal BankAccount4(AccountType accountType) : this()
+        {
+            _accountType = accountType;
+        }
+        internal BankAccount4(decimal balance, AccountType accountType) : this()
+        {
+            _balance = balance;
+            _accountType = accountType;
+        }
+
+        internal int SetBalance
+        {
+            set
+            {
+                this._balance = value;
+            }
+        }
+
+        internal int SetAccountType
+        {
+            set
+            {
+                this._accountType = (AccountType)value;
+            }
+        }
+        
+        internal void PrintAccountDetails()
+        {
+            Console.WriteLine("Информация о счете:");
+            Console.WriteLine("Номер счета: " + _accountId);
+            Console.WriteLine("Баланс: " + _balance);
+            Console.WriteLine("Тип счета: " + _accountType);
+        }
+        private void GenerateAccountId()
+        {
+            _accountId = ++_id;
+        }
+    }
+}
